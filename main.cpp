f0384e5b374b398151be13adf50c08e475d3cfbb
TonyLiu <ltsplice@gmail.com>
2023-03-09 12:42:15 -0500

    did lab7

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string removeLeadingSpaces(string);
+int countChar(string line, char c);
+
+int main(){
+    ifstream fin("bad.cpp");
+    int open_brackets = 0;
+    string a;
+    while(getline(fin,a)){
+        bool close = false;
+        a=removeLeadingSpaces(a);
+        if(a[0] == '}'){
+            close = true;
+            open_brackets -= 1;}
+        for(int i = 0 ; i < open_brackets; i++){
+            cout <<"\t";
+        }
+        cout<<a<<"\n";
+        open_brackets += countChar(a,'{');
+        if(!(close)){
+            open_brackets -= countChar(a,'}');
+        }
+    }
+}
+int countChar(string line, char c){
+    int i = 0;
+    for(char x : line){
+        if(x == c){
+            i++;
+        }
+    }
+    return i;
+}
+
+string removeLeadingSpaces(string line){
+    string ret = "";
+    for(int i = 0;i < line.length();i++){
+        if(isspace(line[i])){
+            continue;
+        }
+        else{
+            ret = line.substr(i,line.length()-i);
+            break;
+        }
+    }
+    return ret;
+}
